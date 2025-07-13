afe2eab4de6b5c62e3917063886fe1c396fc95a6
Diogobq7 <diogo.queiroz@germinare.org.br>
2025-07-12 23:37:50 -0300

    feat: criando a splash screen

--- app/src/main/java/com/aula/app_fluxar/ui/MainActivity.kt
+++ app/src/main/java/com/aula/app_fluxar/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.aula.app_fluxar
+package com.aula.app_fluxar.ui
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.aula.app_fluxar.R
 import com.aula.app_fluxar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -26,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
