ae93fdc7e395a07698c66cf22c91cc872cb3547d
Diogobq7 <diogo.queiroz@germinare.org.br>
2025-07-28 19:57:16 -0300

    feat: adicionando scroll na tela inicial

--- app/src/main/java/com/aula/app_fluxar/ui/fragment/NavigationHome.kt
+++ app/src/main/java/com/aula/app_fluxar/ui/fragment/NavigationHome.kt
@@ -13,54 +13,54 @@ import com.aula.app_fluxar.R
 import com.google.android.material.button.MaterialButton
 
 class NavigationHome : Fragment() {
-    private lateinit var btTelaInicial: Button
-    private lateinit var btCadastrar: Button
-    private lateinit var btRemover: Button
-    private lateinit var conteudo: FrameLayout
+    private lateinit var homeScreenButton: Button
+    private lateinit var registerButton: Button
+    private lateinit var removeButton: Button
+    private lateinit var content: FrameLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_nav_home, container, false)
 
-        btTelaInicial = view.findViewById(R.id.bt_info_gerais)
-        btCadastrar = view.findViewById(R.id.bt_adicionar_estoque)
-        btRemover = view.findViewById(R.id.bt_remover_estoque)
-        conteudo = view.findViewById(R.id.container_conteudo)
+        homeScreenButton = view.findViewById(R.id.bt_info_gerais)
+        registerButton = view.findViewById(R.id.bt_adicionar_estoque)
+        removeButton = view.findViewById(R.id.bt_remover_estoque)
+        content = view.findViewById(R.id.container_conteudo)
 
-        mostrarConteudo(R.layout.fragment_layout_home)
-        atualizarBotoesSelecionados(btTelaInicial)
+        showContent(R.layout.fragment_layout_home)
+        updateSelectedButtons(homeScreenButton)
 
-        btTelaInicial.setOnClickListener {
-            mostrarConteudo(R.layout.fragment_layout_home)
-            atualizarBotoesSelecionados(btTelaInicial)
+        homeScreenButton.setOnClickListener {
+            showContent(R.layout.fragment_layout_home)
+            updateSelectedButtons(homeScreenButton)
         }
 
-        btCadastrar.setOnClickListener {
-            mostrarConteudo(R.layout.fragment_layout_cadastrar_produto)
-            atualizarBotoesSelecionados(btCadastrar)
+        registerButton.setOnClickListener {
+            showContent(R.layout.fragment_layout_cadastrar_produto)
+            updateSelectedButtons(registerButton)
         }
 
-        btRemover.setOnClickListener {
-            mostrarConteudo(R.layout.fragment_layout_remover_produto)
-            atualizarBotoesSelecionados(btRemover)
+        removeButton.setOnClickListener {
+            showContent(R.layout.fragment_layout_remover_produto)
+            updateSelectedButtons(removeButton)
         }
 
         return view
 
     }
 
-    private fun mostrarConteudo(layoutId: Int) {
+    private fun showContent(layoutId: Int) {
         val inflater = LayoutInflater.from(requireContext())
-        conteudo.removeAllViews()
-        val novoConteudo = inflater.inflate(layoutId, conteudo, false)
-        conteudo.addView(novoConteudo)
+        content.removeAllViews()
+        val novoConteudo = inflater.inflate(layoutId, content, false)
+        content.addView(novoConteudo)
     }
 
-    private fun atualizarBotoesSelecionados(botaoSelecionado: Button) {
-        val botoes = listOf(btTelaInicial, btCadastrar, btRemover)
+    private fun updateSelectedButtons(selectedButton: Button) {
+        val botoes = listOf(homeScreenButton, registerButton, removeButton)
         botoes.forEach { botao ->
-            if (botao == botaoSelecionado) {
+            if (botao == selectedButton) {
                 botao.setBackgroundResource(R.drawable.bt_ativo)
                 botao.setTextColor(Color.WHITE)
             } else {
