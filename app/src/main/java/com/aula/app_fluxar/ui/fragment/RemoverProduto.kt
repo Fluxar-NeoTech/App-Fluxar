4a6ba38a8056a719d35b13b565578757c0e43834
Diogobq7 <diogo.queiroz@germinare.org.br>
2025-08-04 11:22:04 -0300

    fix: arrumando o nome das classes

--- app/src/main/java/com/aula/app_fluxar/ui/fragment/RemoverProduto.kt
+++ app/src/main/java/com/aula/app_fluxar/ui/fragment/RemoverProduto.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [layout_home.newInstance] factory method to
+ * Use the [RemoverProduto.newInstance] factory method to
  * create an instance of this fragment.
  */
-class layout_home : Fragment() {
+class RemoverProduto : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class layout_home : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_layout_home, container, false)
+        return inflater.inflate(R.layout.fragment_layout_remover_produto, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class layout_home : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment layout_home.
+         * @return A new instance of fragment layout_remover_produto.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            layout_home().apply {
+            RemoverProduto().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
