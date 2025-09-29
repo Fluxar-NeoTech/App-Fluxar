47e0a1b9dc59ccf0f38d17e62271238a53f1a052
Diogobq7 <diogo.queiroz@germinare.org.br>
2025-09-29 10:43:40 -0300

    feat: adicionando enpoint de cadastro de capacidade do estoque

--- app/src/main/java/com/aula/app_fluxar/API/service/APIService.kt
+++ app/src/main/java/com/aula/app_fluxar/API/service/APIService.kt
@@ -1,5 +1,7 @@
 package com.aula.app_fluxar.API.service
 
+import com.aula.app_fluxar.API.model.CapacityStockRequest
+import com.aula.app_fluxar.API.model.CapacityStockResponse
 import com.aula.app_fluxar.API.model.Employee
 import com.aula.app_fluxar.API.model.LoginRequest
 import com.aula.app_fluxar.API.model.UpdatePhotoRequest
@@ -14,4 +16,7 @@ interface APIService {
 
     @PUT("api/employee/update/photo")
     suspend fun updatePhoto(@Body employee: UpdatePhotoRequest): Response<Map<String, String>>
+
+    @POST("/api/capacityStock/addCapacityStock")
+    suspend fun addCapacityStock(@Body capacityStockRequest: CapacityStockRequest): Response<CapacityStockResponse>
 }
